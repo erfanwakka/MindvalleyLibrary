e3696c25af918833faebf55a9387f63b7e4bf4b6
erfan <andesta.erfan@gmail.com>
2019-06-28 15:41:59 +0430

    create ImageProfile struct

---
+++ MindvalleyImageLoader/MainView/Model/ImageProfile.swift
@@ -0,0 +1,16 @@
+//
+//  ImageProfile.swift
+//  MindvalleyImageLoader
+//
+//  Created by erfan on 4/7/1398 AP.
+//  Copyright © 1398 erfan. All rights reserved.
+//
+
+import Foundation
+
+
+struct ImageProfile: Decodable {
+    var small: String
+    var medium: String
+    var large: String
+}
