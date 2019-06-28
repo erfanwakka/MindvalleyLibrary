40b3bc996759a97b3131d44a2e156de20f123de5
erfan <andesta.erfan@gmail.com>
2019-06-28 15:54:16 +0430

    create UrlProfile model

---
+++ MindvalleyImageLoader/MainView/Model/UrlProfile.swift
@@ -0,0 +1,18 @@
+//
+//  UrlProfile.swift
+//  MindvalleyImageLoader
+//
+//  Created by erfan on 4/7/1398 AP.
+//  Copyright © 1398 erfan. All rights reserved.
+//
+
+import Foundation
+
+
+struct UrlProfile: Decodable {
+    var raw: String?
+    var full: String?
+    var regular: String?
+    var small: String?
+    var thumb: String?
+}
