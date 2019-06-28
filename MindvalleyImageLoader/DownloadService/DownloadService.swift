e7043778f0fb178c0b793657b52c797bd548405d
erfan <andesta.erfan@gmail.com>
2019-06-28 16:54:08 +0430

    implement simple network API

--- MindvalleyImageLoader/DownloadService/DownloadService.swift
+++ MindvalleyImageLoader/DownloadService/DownloadService.swift
@@ -6,4 +6,47 @@
 //  Copyright © 1398 erfan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class DownloadService {
+    
+    
+    private init() {}
+    static var shared = DownloadService()
+    
+    func downloadImage(with url: URL, completionHandler: @escaping (_ data: UIImage?) -> Void) {
+        URLSession.shared.dataTask(with: url) { (data, res, error) in
+            if error == nil, let unrwappedData = data {
+                let image = UIImage(data: unrwappedData)
+                DispatchQueue.main.async {
+                    completionHandler(image)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    completionHandler(nil)
+                }
+            }
+            }.resume()
+    }
+    func GetRequest(with url: URL, completionHandler: @escaping (_ response: [Image]?) -> Void) {
+        URLSession.shared.dataTask(with: url) { (data, res, error) in
+            if error == nil, let unrwappedData = data {
+                let decoder = JSONDecoder()
+                do {
+                    let images = try decoder.decode([Image].self, from: unrwappedData)
+                    DispatchQueue.main.async {
+                        completionHandler(images)
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        completionHandler(nil)
+                    }
+                }
+            } else {
+                DispatchQueue.main.async {
+                    completionHandler(nil)
+                }
+            }
+            }.resume()
+    }
+}
--- MindvalleyImageLoader/MainView/Controller/PinboardVC.swift
+++ MindvalleyImageLoader/MainView/Controller/PinboardVC.swift
@@ -18,11 +18,18 @@ class PinboardVC: UIViewController {
     
     //MARK: - Vars -
     
+    var images: [Image]? {
+        didSet {
+            PinboardCollectionView.reloadData()
+        }
+    }
+    
     //MARK: - View LifeCycle's -
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configView()
+        getDatas()
     }
     
     //MARK: - Functions -
@@ -32,19 +39,33 @@ class PinboardVC: UIViewController {
         PinboardCollectionView.dataSource = self
         PinboardCollectionView.register(UINib(nibName: Constants.CellIdentifiers.regularCell, bundle: nil), forCellWithReuseIdentifier: Constants.CellIdentifiers.regularCell)
     }
+    private func getDatas() {
+        DownloadService.shared.GetRequest(with: URL(string: URLs.base)!) { (images) in
+            self.images = images
+        }
+    }
+    private func downloadImage(with fileName: String, completionHandler: @escaping (_ image: UIImage?) -> Void) {
+        DownloadService.shared.downloadImage(with: URL(string: fileName)!) { (image) in
+            completionHandler(image)
+        }
+    }
 }
 
 // MARK: -  PinboardVC collection delegate  -
 extension PinboardVC: UICollectionViewDelegate {
     
+    
 }
 // MARK: -  PinboardVC collection dataSource  -
 extension PinboardVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return images?.count ?? 0
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.CellIdentifiers.regularCell, for: indexPath) as? RegularCell else { return UICollectionViewCell() }
+        downloadImage(with: (images![indexPath.item].urls?.regular)!) { (image) in
+            cell.image.image = image
+        }
         return cell
     }
 }
