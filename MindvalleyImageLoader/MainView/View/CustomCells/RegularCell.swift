748e30279ed138ac235525f525e102f5da541946
erfan <andesta.erfan@gmail.com>
2019-06-28 16:17:55 +0430

    add regular custom cell to show pinterest image

---
+++ MindvalleyImageLoader/MainView/View/CustomCells/RegularCell.swift
@@ -0,0 +1,22 @@
+//
+//  squareCell.swift
+//  MindvalleyImageLoader
+//
+//  Created by erfan on 4/7/1398 AP.
+//  Copyright © 1398 erfan. All rights reserved.
+//
+
+import UIKit
+
+class RegularCell: UICollectionViewCell {
+    
+    //MARK: - Outlets -
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    //MARK: - Actions -
+    
+    
+    //MARK: - Functions -
+
+}
