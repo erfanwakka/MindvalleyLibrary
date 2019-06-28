ec7cd3486003ddc3ec9422e1e2f16d1cb90ecee7
erfan <andesta.erfan@gmail.com>
2019-06-28 15:54:42 +0430

    create Image model

---
+++ MindvalleyImageLoader/MainView/Model/Image.swift
@@ -0,0 +1,25 @@
+//
+//  Image.swift
+//  MindvalleyImageLoader
+//
+//  Created by erfan on 4/7/1398 AP.
+//  Copyright © 1398 erfan. All rights reserved.
+//
+
+import Foundation
+
+
+struct Image: Decodable {
+    var id: String?
+    var created_at: String?
+    var width: Int?
+    var height: Int?
+    var color: String?
+    var likes: Int?
+    var liked_by_user: Bool?
+    var user: user
+    var current_user_collections: [String] // all of the arrays was empty so I assume it is array of string
+    var urls: UrlProfile?
+    var categories: [Category]
+    var links: Links
+}
