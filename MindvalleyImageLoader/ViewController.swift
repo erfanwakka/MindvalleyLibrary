1c0b08f6046e1e7cb53b4ddb8a1678ccdc84cdb8
erfan <andesta.erfan@gmail.com>
2019-06-28 15:14:09 +0430

    Initial Commit

---
+++ MindvalleyImageLoader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MindvalleyImageLoader
+//
+//  Created by erfan on 4/7/1398 AP.
+//  Copyright © 1398 erfan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
