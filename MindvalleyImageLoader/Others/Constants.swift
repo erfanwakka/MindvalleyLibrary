c0e592fdd8a3cbcf33e644ad73cab38606543e97
erfan <andesta.erfan@gmail.com>
2019-06-28 16:17:34 +0430

    add Constant file for having our constant values

---
+++ MindvalleyImageLoader/Others/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  MindvalleyImageLoader
+//
+//  Created by erfan on 4/7/1398 AP.
+//  Copyright © 1398 erfan. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    struct CellIdentifiers {
+        static var regularCell = "RegularCell"
+    }
+}
