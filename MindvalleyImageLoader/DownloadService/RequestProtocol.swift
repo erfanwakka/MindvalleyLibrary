6015999210aa6475c28db1b997797fc85a070e41
erfan <andesta.erfan@gmail.com>
2019-06-29 17:17:17 +0430

    add RequestProtocol to handle a request

---
+++ MindvalleyImageLoader/DownloadService/RequestProtocol.swift
@@ -0,0 +1,33 @@
+//
+//  RequestProtocol.swift
+//  MindvalleyImageLoader
+//
+//  Created by erfan on 4/8/1398 AP.
+//  Copyright © 1398 erfan. All rights reserved.
+//
+
+import UIKit
+
+public protocol RequestProtocol {
+    
+    associatedtype CodableType: Decodable
+}
+
+public extension RequestProtocol {
+    
+    func executeRequest(dataRequest: Request, requestManager: Network = DownloadService.shared, onSuccess: @escaping (CodableType) -> (), onError: @escaping (Error) -> ()) {
+        requestManager.request(with: dataRequest, onSuccess: { (data) in
+            let decoder = JSONDecoder()
+            do {
+                let object = try decoder.decode(CodableType.self, from: data)
+                onSuccess(object)
+            } catch {
+                onError(error)
+            }
+        }) { (error) in
+            onError(error)
+        }
+    }
+}
+
+
