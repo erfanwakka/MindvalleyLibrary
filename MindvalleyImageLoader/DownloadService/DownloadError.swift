4b96fc3bd779706587a7f64de2841e6c16458731
erfan <andesta.erfan@gmail.com>
2019-06-29 10:43:47 +0430

    add Download Error enum

---
+++ MindvalleyImageLoader/DownloadService/DownloadError.swift
@@ -0,0 +1,14 @@
+//
+//  Error.swift
+//  MindvalleyImageLoader
+//
+//  Created by erfan on 4/7/1398 AP.
+//  Copyright © 1398 erfan. All rights reserved.
+//
+
+import Foundation
+
+public enum DownloadError: Error {
+    case noData
+    case invalidURL
+}
